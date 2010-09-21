53bcdc22c0c7c84b6905c41e0517cedb2116a4aa
kaitlin <kaitlin@kaitlin-work.(none)>
2010-09-21 15:25:20 -0400

    turned into an actual package

---
+++ README.txt
@@ -0,0 +1,11 @@
+======
+Charty
+======
+
+Charty is a python library that generates SVG charts that use cascading stylesheets to alter their style and appearance. 
+
+
+
+Keyword Argument Options
+========================
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Charty',
+    version='0.1.0',
+    author='Kaitlin Lee',
+    author_email='klee@sunlightfoundation.com',
+    packages=['charty'],
+    url='http://pypi.python.org/pypi/Charty/',
+    license='LICENSE.txt',
+    description='Another Python Chart Generator that uses CSS smartly',
+    long_description=open('README.txt').read(),
+)
+
